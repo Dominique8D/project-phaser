fa95fb717b4c675b7314ea6f0365434325eb97aa
Dominique8D <dejong.dominique@gmail.com>
2025-08-13 15:02:59 +0000

    Add jump feature

--- src/endless_game/objects/player.ts
+++ src/endless_game/objects/player.ts
@@ -3,6 +3,8 @@ import Phaser from 'phaser';
 export class Player extends Phaser.Physics.Arcade.Sprite {
   private leftBound: number;
   private rightBound: number;
+  private bodyRef: Phaser.Physics.Arcade.Body;
+  private readonly jumpPower: number = 500;
 
   constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
     super(scene, x, y, texture);
@@ -12,11 +14,18 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
 
     this.setCollideWorldBounds(true);
 
+    this.bodyRef = this.body as Phaser.Physics.Arcade.Body;
+    this.setBounce(0.5);
+
     const screenWidth = scene.scale.width;
     const margin = screenWidth * 0.025;
 
     this.leftBound = margin + this.width / 2;
     this.rightBound = screenWidth - margin - this.width / 2;
+
+    scene.input.on('pointerdown', () => {
+      this.jump();
+    });
   }
 
   public update(pointer?: Phaser.Input.Pointer) {
@@ -24,6 +33,12 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
 
     if (pointer.worldX >= this.leftBound && pointer.worldX <= this.rightBound) {
       this.setX(pointer.worldX);
+    }
+  }
+
+  private jump() {
+    if (this.bodyRef.blocked.down) {
+      this.setVelocityY(-this.jumpPower);
     }
   }
 }
