4b0b1d0df6e23e063a7f43ae11b4404526266e2f
Dominique8D <dejong.dominique@gmail.com>
2025-08-13 14:41:53 +0000

    Limit xMovement player

--- src/endless_game/objects/player.ts
+++ src/endless_game/objects/player.ts
@@ -1,6 +1,9 @@
 import Phaser from 'phaser';
 
 export class Player extends Phaser.Physics.Arcade.Sprite {
+  private leftBound: number;
+  private rightBound: number;
+
   constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
     super(scene, x, y, texture);
 
@@ -8,11 +11,20 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
     scene.physics.add.existing(this);
 
     this.setCollideWorldBounds(true);
+
+    const screenWidth = scene.scale.width;
+    const margin = screenWidth * 0.025;
+
+    this.leftBound = margin + this.width / 2;
+    this.rightBound = screenWidth - margin - this.width / 2;
   }
 
   public update(pointer?: Phaser.Input.Pointer) {
     if (!pointer) return;
-    this.setX(pointer.worldX);
+
+    if (pointer.worldX >= this.leftBound && pointer.worldX <= this.rightBound) {
+      this.setX(pointer.worldX);
+    }
   }
 }
 
