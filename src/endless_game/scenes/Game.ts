158cd554acd2affdd468d054811b34fbac4290d5
Dominique8D <dejong.dominique@gmail.com>
2025-08-21 13:31:59 +0000

    Moving camera and higher top

--- src/endless_game/scenes/Game.ts
+++ src/endless_game/scenes/Game.ts
@@ -3,10 +3,10 @@ import { Scene } from 'phaser';
 import { Player } from '../objects/player';
 import { JumpOrb } from '../objects/jump-orb';
 
+const WORLD_HEIGHT = 1000;
+
 export class Game extends Scene {
   camera: Phaser.Cameras.Scene2D.Camera;
-  background: Phaser.GameObjects.Image;
-  gameText: Phaser.GameObjects.Text;
   player: Player;
   pointer: Phaser.Input.Pointer;
 
@@ -16,26 +16,57 @@ export class Game extends Scene {
 
   preload() {
     this.load.image('tst_idle', 'assets/tst_idle.png');
+    this.load.image('tst_powerup', 'assets/tst_powerup.png');
   }
 
   create() {
+    const screenWidth = this.scale.width;
+    const screenHeight = this.scale.height;
+
+    const worldWidth = screenWidth;
+    const worldHeight = WORLD_HEIGHT;
+
+    this.physics.world.setBounds(0, 0, worldWidth, worldHeight);
+
     this.camera = this.cameras.main;
-    this.camera.setBackgroundColor(0x00ff00);
+    this.camera.setBounds(0, 0, worldWidth, worldHeight);
+    // Start camera at bottom
+    this.camera.scrollY = worldHeight - screenHeight;
 
-    this.background = this.add.image(512, 384, 'background');
-    this.background.setAlpha(0.5);
+    // Debug lines for ground and top
+    const groundY = worldHeight;
 
-    this.physics.world.setBounds(0, 0, 1024, 768);
+    const groundLine = this.add.graphics();
+    groundLine.lineStyle(20, 0xff0000, 1);
+    groundLine.beginPath();
+    groundLine.moveTo(0, groundY);
+    groundLine.lineTo(worldWidth, groundY);
+    groundLine.strokePath();
 
-    const scaleWidth = this.scale.width;
-    const scaleHeight = this.scale.height;
+    const topLine = this.add.graphics();
+    topLine.lineStyle(20, 0x00ff00, 1);
+    topLine.beginPath();
+    topLine.moveTo(0, 0);
+    topLine.lineTo(worldWidth, 0);
+    topLine.strokePath();
 
-    this.player = new Player(this, scaleWidth / 2, scaleHeight / 1, 'tst_idle');
+    // Start player at bottom
+    const playerStartX = screenWidth / 2;
+    const playerStartY = groundY - 100;
+
+    this.player = new Player(this, playerStartX, playerStartY, 'tst_idle');
     this.player.setScale(5);
 
-    new JumpOrb(this, scaleWidth / 2, 550, 'tst_powerup', this.player).setScale(5);
-    new JumpOrb(this, scaleWidth / 1.5, 350, 'tst_powerup', this.player).setScale(5);
-    new JumpOrb(this, scaleWidth / 2.5, 150, 'tst_powerup', this.player).setScale(5);
+    this.camera.startFollow(this.player, false, 0, 1);
+
+    // Powerups test placement
+    new JumpOrb(this, playerStartX, playerStartY - 200, 'tst_powerup', this.player).setScale(5);
+    new JumpOrb(this, playerStartX + 150, playerStartY - 400, 'tst_powerup', this.player).setScale(
+      5,
+    );
+    new JumpOrb(this, playerStartX - 150, playerStartY - 600, 'tst_powerup', this.player).setScale(
+      5,
+    );
 
     this.input.on('pointermove', (pointer: Phaser.Input.Pointer) => {
       this.pointer = pointer;
@@ -44,7 +75,9 @@ export class Game extends Scene {
     EventBus.emit('current-scene-ready', this);
   }
 
-  update() {
+  update(delta: number) {
+    const camScrollSpeedPerSec = 120;
     this.player.update(this.pointer);
+    this.camera.scrollY -= camScrollSpeedPerSec * (delta / 1000);
   }
 }
